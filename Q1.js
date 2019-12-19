52a181ee24121feeed2703ecbd0a0958ebaed652
willy0077 <willy633526@gmail.com>
2019-12-19 10:20:48 +0800

    Q1

---
+++ Q1.js
@@ -0,0 +1,19 @@
+// function reversechar(string) {
+//     return string.split("").reverse().join("");
+// }
+
+// var str = "abcdef";
+// var str2 = "abcdef cow apple";
+// var strtemp = "";
+
+// for (let i = 0; i < 3; i++) {
+//     strtemp += reversechar(str2.split(" ")[i]) + " ";
+// }
+
+// str2 = strtemp;
+// console.log(str2);
+
+var strtemp = "";
+
+strtemp = str2.split(" ");
+console.log(strtemp);
